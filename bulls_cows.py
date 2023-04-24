15446e4ca879b9b487a2436804a594890e123ad0
MatejFrolik <matejfrolik1@seznam.cz>
2023-04-24 12:52:43 +0200

    Def main function for whole program.
    bulls_cows_game include all previos functions and run whole game.

--- bulls_cows.py
+++ bulls_cows.py
@@ -12,15 +12,15 @@ def num_generator() -> str:
     Return random 4digits number with 
     different numbers. It doesn't start with 0.
     '''
-    run = True
-    while run:
+    while True:
         num_gen = random.sample('0123456789', 4)
    #     num_gen = ''.join(num_gen)
         if num_gen[0] == '0':
             continue
         else:
             return ''.join(num_gen)
-    run = False
+
+print(num_generator())
 
 def user_guess() -> str:   
     '''
@@ -28,8 +28,7 @@ def user_guess() -> str:
     User input 4digits number. User is not allowed 
     to insert longer number or other characters. 
     '''
-    run = True
-    while run:
+    while True:
         guess = input('Enter a 4 digits number which doesn\'t start with 0: ')
         if len(guess) != 4 or not guess.isdigit() or guess[0] == '0':
             print('Wrong input, please enter 4 digits number one more time')
@@ -43,12 +42,41 @@ def count_bulls_cows(number,guess) -> int:
     '''
     bulls = 0
     cows = 0
-    for x in range(len(user_guess())):
+    for x in range(len(guess)):
         if guess[x] == number[x]:
             bulls += 1
         elif guess[x] in number:
             cows += 1
-    return (cows,bulls)
+    return (bulls, cows)
 
 
 
+def bulls_cows_game() -> None:
+    '''
+    Description:
+    The main function for running whole program.
+    '''
+
+#   rules_greeting.bulls_cows
+#   rules_greeting.separator
+#   rules_greeting.rules
+#   rules_greetign.separator
+    number = num_generator()
+    guesses = 0
+    t1 = time.perf_counter()
+    program = True
+    while program :
+        t1 = time.perf_counter()
+        user_input = user_guess()
+        guesses += 1
+        cows,bulls = count_bulls_cows(number,user_input)
+        print(f'cows: {cows} bulls: {bulls}')
+        if bulls == 4: 
+            print(f'You entered correct number. Number o attempts: {guesses}')
+            break
+    t2 = time.perf_counter()
+    took_time = (t2-t1)
+    print(f'Time: {took_time}')
+        
+
+bulls_cows_game()
