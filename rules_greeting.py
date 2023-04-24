bd4fa6858701b94e560f5ea02d71e2f4d2cdcefd
MatejFrolik <matejfrolik1@seznam.cz>
2023-04-24 10:52:27 +0200

    nothing else

--- rules_greeting.py
+++ rules_greeting.py
@@ -1,11 +1,14 @@
-rules = print('''RULES:
+rules = print('''
+
+RULES:
 1. The entered number must have only 4 differet digits. No other characters or more digits.
 2. If the place of the one digit is same as the place of the random computers\'s number - it\'s Bull.
 3. If the place of the one digit is same as the place of the random computer\'s number - is\'s Cow.
 Try your best and have a fun
+
 ''')
 
-bulls_cows = print("""¨
+bulls_cows = print("""
   _ _ _              __  __               _ _ _
  |  __  \           |  ||  |            /   _ _|
  | |  |  |          |  ||  |           /  /
@@ -23,4 +26,5 @@ bulls_cows = print("""¨
 if __name__ == '__main__':
     print(rules)
     print(bulls_cows)
+
 
