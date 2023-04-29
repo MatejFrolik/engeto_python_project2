50c7ab073609e7d636ca53c96a88293f708fa1ad
MatejFrolik <matejfrolik1@seznam.cz>
2023-04-29 17:36:55 +0200

    Correction rules_greeting.py module.

--- rules_greeting.py
+++ rules_greeting.py
@@ -1,14 +1,12 @@
-rules = print('''
-
+rules = '''
 RULES:
 1. The entered number must have only 4 differet digits. No other characters or more digits.
 2. If the place of the one digit is same as the place of the random computers\'s number - it\'s Bull.
 3. If the place of the one digit is same as the place of the random computer\'s number - is\'s Cow.
 Try your best and have a fun
+'''
 
-''')
-
-bulls_cows = print("""
+bulls_cows = """
   _ _ _              __  __               _ _ _
  |  __  \           |  ||  |            /   _ _|
  | |  |  |          |  ||  |           /  /
@@ -20,13 +18,10 @@ bulls_cows = print("""
  | |   |  ||  | |  ||  ||  | \_ _   | \  \      |  \     /  |  \  \/  /     \  \/  /    \_ _   |
  | |__/  / |  \ /  ||  ||  ||       |  \  \_ _ _ \  \ _ /  /    \    /       \    /    |       |
  |_ _ _ /   \_ _ _/ \__/\__/|_ _ _ _/   \_ _ _ _| \_ _ _ _/      \__/         \__/     |_ _ _ _/
-
- """)
-
-separator = '='*50
+ """
 
 if __name__ == '__main__':
     print(rules)
     print(bulls_cows)
-    print(oddelovac)
+
 
